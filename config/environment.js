0561f1d5e8afe5b6016f1b94dd0e3ff9cb7f8ab6
kenprogrammer <kenprogrammer@gmail.com>
2022-03-14 17:59:58 +0300

    Tutorial Part 1

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,8 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1Ijoia2VucHJvZ3JhbW1lciIsImEiOiJjbDBxcTBlNHkwMWVoM2Rwa2VwYmF1OWpiIn0.lmwmLInWP9xyNQNCWLZSRg';
+
   return ENV;
 };
